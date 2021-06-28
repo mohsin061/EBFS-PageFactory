42b5e650ed4c8819646296b3f3a82e0febd02710
mohsino61 <mohsin.wilmu@gmail.com>
2021-06-27 22:54:33 -0400

    Test case 2

--- src/main/java/com/ebfs/qa/properties/TestBaseProperties.java
+++ src/main/java/com/ebfs/qa/properties/TestBaseProperties.java
@@ -5,8 +5,8 @@ public interface TestBaseProperties {
 	public final String CONFIG_PATH = System.getProperty("user.dir")+ "/src/main/java/com/ebfs/qa/config/config.properties"; 
 
 	public final String CHROME_DRIVER = "webdriver.chrome.driver";
-	public final String CHROME_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver.exe";
+	public final String CHROME_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver";
 	public final String FIREFOX_DRIVER = "webdriver.gecko.driver";
-	public final String FIREFOX_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/geckodriver.exe";
+	public final String FIREFOX_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/geckodriver";
 	
 }
